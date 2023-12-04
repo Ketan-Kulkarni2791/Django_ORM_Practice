f04a02a6e9ad72f5715bcc7d7a43ce5aacc92183
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2023-12-04 08:46:24 +0530

    department table created

--- employee_management_system/ems_dev/models.py
+++ employee_management_system/ems_dev/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    department_id = models.AutoField(primary_key=True, default=100)
+    department_name = models.CharField(max_length=100, blank=False, null=False)
+    
+    class Meta:
+        db_table = 'dev_schema"."department'
+    
+    def __str__(self) -> str:
+        return self.department_name
